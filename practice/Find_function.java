ac11b8a782e87b81baaf78b6e8dc53327b8b88af
aditya chinchalkar <91894161+adityachinchalkar@users.noreply.github.com>
2021-10-24 11:51:30 +0530

    Add files via upload

---
+++ practice/Find_function.java
@@ -0,0 +1,16 @@
+package practice;
+
+public class Find_function {
+
+
+        public static void main(String args[])
+        {
+          //1. Function to find the sum of 2 numbers
+            int x = 225,  y= 115, sum;
+            sum = x + y;
+            System.out.println("The sum of numbers is: "+sum);
+        }
+        
+    }
+
+
