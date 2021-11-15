444e4227b0553eff91a45fa82862eeb6d9ff8321
aditya chinchalkar <91894161+adityachinchalkar@users.noreply.github.com>
2021-11-15 09:31:29 +0530

    Add files via upload

---
+++ AssignmentNO5/Employee.java
@@ -0,0 +1,25 @@
+package AssignmentNO5;
+
+import java.util.ArrayList;
+
+public class Employee {
+
+    int empId;
+    float salary;
+   int Datedob;
+   int DatedateOfJoining;
+    String name;
+
+
+   public void Department{
+
+        String deptName;
+
+        List<Employee> employees = new ArrayList<Employee>();
+        employees.add = "varun raut";
+        employees.add = "kunal";
+        
+
+    }
+}
+
