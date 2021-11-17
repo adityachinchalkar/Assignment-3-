ad5196017bf4dcbe2fdf2fb774230125636f0fc1
aditya chinchalkar <91894161+adityachinchalkar@users.noreply.github.com>
2021-11-17 09:06:33 +0530

    Add files via upload

--- AssignmentNO5/Employee.java
+++ AssignmentNO5/Employee.java
@@ -1,25 +1,37 @@
 package AssignmentNO5;
 
-import java.util.ArrayList;
-
 public class Employee {
+    public static void main(String args[]) {
+        EmployeeDetails emp = new EmployeeDetails();
+        emp.setEmp_id(101);
+        emp.setName("Varun Raut");
+        emp.setDepartment("IT");
+        emp.setSalary(15000);
+        emp.setAddress("Pune Maharashtra");
+        emp.setEmail("Varun21raut@gmail.com");
+        System.out.println(emp);
 
-    int empId;
-    float salary;
-   int Datedob;
-   int DatedateOfJoining;
-    String name;
+        int sal = emp.getSalary();
+        int increment = 0;
+        if ((sal >=1000) && (sal <=1500))
+        {
+            increment += (sal * 2)/100;
+            sal = sal+increment;
 
+            emp.setSalary(sal);
+            System.out.println("\n Salary is incremented \n");
+            System.out.println(emp);
 
-   public void Department{
-
-        String deptName;
-
-        List<Employee> employees = new ArrayList<Employee>();
-        employees.add = "varun raut";
-        employees.add = "kunal";
-        
-
+        }else if ((sal >=1500) && (sal <=20000)){
+            increment += (sal * 5)/100;
+            sal = sal+increment;
+            emp.setSalary(sal);
+            System.out.println("\n Salary is incremented \n");
+            System.out.println(emp);
+        }else {
+            System.out.println("\n Salary is not incremented \n");
+            System.out.println(emp);
+        }
     }
 }
 
